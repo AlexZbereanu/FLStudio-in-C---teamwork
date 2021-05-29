fdaa61ffc0206288b5493e1268d88e8d54f1dd2a
AlexZbereanu <57541189+AlexZbereanu@users.noreply.github.com>
2021-05-29 22:32:57 +0300

    Add files via upload

---
+++ Note/Note.cs
@@ -0,0 +1,80 @@
+﻿/********************************************************************************
+ *                                                                              *
+ *  File:        Note.cs                                                        *
+ *  Copyright:   (c) 2021, A.A. Harpa, M.S. Sirghi, A.B. Stoian, A. Zbereanu    *
+ *  Description: Describe all informations about a musical note.                *
+ *                                                                              *
+ *  This code and information is provided "as is" without warranty of any kind, *
+ *  either expressed or implied, including but not limited to the implied       *
+ *  warranties of merchantability or fitness for a particular purpose. You are  *
+ *  free to use this source code in your applications as long as the original   *
+ *  copyright notice is included.                                               *
+ *                                                                              *
+ ********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note
+{
+    /// <summary>
+    /// Class <c>Note</c> hold the informations about a musical note.
+    /// <param name="_position">Coords of a note block.</param>
+    /// <param name="_pathToNote">Path to a particular sound.</param>
+    /// <param name="_noteColor">Color of a note block.</param>
+    /// </summary>
+    public class Note
+    {
+        private Point _position;
+        private string _pathToNote;
+        private Color _noteColor;
+
+        /// <summary>
+        /// Init constructor of a Note class. 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="position"></param>
+        /// <param name="color"></param>
+        public Note(string path, Point position, Color color)
+        {
+            _position = position;
+            _pathToNote = path;
+            _noteColor = color;
+        }
+
+        /// <summary>
+        /// Getter for x position of a note.
+        /// </summary>
+        public int XPosition
+        {
+            get
+            {
+                return _position.X;
+            }
+        }
+        /// <summary>
+        /// Getter for y position of a note.
+        /// </summary>
+        public int YPosition
+        {
+            get
+            {
+                return _position.Y;
+            }
+        }
+
+        /// <summary>
+        /// Getter for path note.
+        /// </summary>
+        public string PathToNote
+        {
+            get
+            {
+                return _pathToNote;
+            }
+        }
+    }
+}
---
+++ Simulation/Simulation.cs
@@ -0,0 +1,95 @@
+﻿/********************************************************************************
+ *                                                                              *
+ *  File:        Simulation.cs                                                  *
+ *  Copyright:   (c) 2021, A.A. Harpa, M.S. Sirghi, A.B. Stoian, A. Zbereanu    *
+ *  Description: Contains informations about the play bar.                      *
+ *                                                                              *
+ *  This code and information is provided "as is" without warranty of any kind, *
+ *  either expressed or implied, including but not limited to the implied       *
+ *  warranties of merchantability or fitness for a particular purpose. You are  *
+ *  free to use this source code in your applications as long as the original   *
+ *  copyright notice is included.                                               *
+ *                                                                              *
+ ********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation
+{
+    /// <summary>
+    /// Class <c>Simulation</c> controls and properties for the play bar.
+    /// <param name="_bar">Rectangle object that represent the bar.</param>
+    /// <param name="_playSpeed">Represent the tempo.</param>
+    /// </summary>
+    public class Simulation
+    {
+        private Rectangle _bar;
+        private int _playSpeed;
+
+        /// <summary>
+        /// Init constructor for <c>Simulation</c> class.
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="playSpeed"></param>
+        public Simulation(int width, int height, int x = 0, int y = 0, int playSpeed = 1)
+        {
+            _bar = new Rectangle(x, y, width, height);
+            _playSpeed = playSpeed;
+        }
+        /// <summary>
+        ///   Getter for the bar object.
+        /// </summary>
+        public Rectangle Bar
+        {
+            get { return _bar; }
+        }
+        /// <summary>
+        /// Getter for x position of the bar.
+        /// </summary>
+        public int GetPlayBarX
+        {
+            get { return _bar.X; }
+        }
+        /// <summary>
+        /// Getter for bar width.
+        /// </summary>
+        public int GetPlayBarWidth
+        {
+            get { return _bar.Width; }
+        }
+        /// <summary>
+        /// Move the bar with an offset.
+        /// </summary>
+        /// <param name="offset"></param>
+        public void MoveBar(int offset)
+        {
+            _bar.X += offset;
+        }
+        /// <summary>
+        /// Resets the bar to initial position.
+        /// </summary>
+        public void Reset()
+        {
+            _bar.X = 0;
+        }
+        /// <summary>
+        /// Getter and setter for  _playSpeed.
+        /// </summary>
+        public int PlaySpeed
+        {
+            get
+            {
+                return _playSpeed;
+            }
+
+            set => _playSpeed = value;
+        }
+    }
+}
